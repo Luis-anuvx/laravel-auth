e932d4a5625297acd71cf2c21f2d0907edf66db0
Luis-anuvx <anuvx.25.011@gmail.com>
2023-08-17 00:17:34 -0400

    crud roles y permisos

--- app/Http/Controllers/PermisoController.php
+++ app/Http/Controllers/PermisoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permiso;
 use Illuminate\Http\Request;
 
 class PermisoController extends Controller
@@ -11,7 +12,8 @@ class PermisoController extends Controller
      */
     public function index()
     {
-        //
+        $permisos =Permiso::get();
+       return response()->json($permisos);
     }
 
     /**
@@ -19,7 +21,14 @@ class PermisoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:permisos"
+        ]);
+        $permiso=new Permiso();
+        $permiso->nombre = $request->nombre;
+        $permiso->detalle = $request->detalle;
+        $permiso->save();
+        return response()->json(["message"=>"Registrado permiso"],201);
     }
 
     /**
@@ -27,7 +36,8 @@ class PermisoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $permiso=Permiso::findOrFail($id);
+        return response()->json($permiso,200);
     }
 
     /**
@@ -35,7 +45,14 @@ class PermisoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:permisos,nombre,$id"
+        ]);
+        $permiso=Permiso::findOrFail($id);
+        $permiso->nombre = $request->nombre;
+        $permiso->detalle = $request->detalle;
+        $permiso->update();
+        return response()->json(["message"=>"Actualizado permiso"],201);
     }
 
     /**
@@ -43,6 +60,8 @@ class PermisoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $permiso=Permiso::findOrFail($id);
+        $permiso->delete();
+        return response()->json(["message"=>"Elimando permiso"],201);
     }
 }
--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
@@ -11,7 +12,8 @@ class RoleController extends Controller
      */
     public function index()
     {
-        //
+       $roles =Role::get();
+       return response()->json($roles);
     }
 
     /**
@@ -19,7 +21,14 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:roles"
+        ]);
+        $role=new Role();
+        $role->nombre = $request->nombre;
+        $role->detalle = $request->detalle;
+        $role->save();
+        return response()->json(["message"=>"Registrado role"],201);
     }
 
     /**
@@ -27,7 +36,8 @@ class RoleController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $rol=Role::findOrFail($id);
+        return response()->json($rol,200);
     }
 
     /**
@@ -35,7 +45,14 @@ class RoleController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            "nombre" => "required|unique:roles,nombre,$id"
+        ]);
+        $role=Role::findOrFail($id);
+        $role->nombre = $request->nombre;
+        $role->detalle = $request->detalle;
+        $role->update();
+        return response()->json(["message"=>"Actualizado role"],201);
     }
 
     /**
@@ -43,6 +60,8 @@ class RoleController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $role=Role::findOrFail($id);
+        $role->delete();
+        return response()->json(["message"=>"Elimando role"],201);
     }
 }
